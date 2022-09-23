782df63d42120272674b7a6fdb7cd43f6e9a3b64
Saravana16041999 <saravanakumar16041999@gmail.com>
2022-09-23 11:53:36 +0530

    basic/ js completed

--- script/script.js
+++ script/script.js
@@ -1,1 +1,50 @@
 'use strict';
+
+const formEl = document.getElementById('Form');
+const usernameEl = document.getElementById('username');
+const emilEl = document.getElementById('mail')
+const numberEl = document.getElementById('number')
+const passwordEl = document.getElementById('password')
+const ConformPasswordel = document.getElementById('ConformPassword')
+
+
+formEl.addEventListener('submit',function (event){
+    event.preventDefault();
+    if(usernameEl.value === ''){
+        let formcontrolEl = usernameEl.parentElement;
+        formcontrolEl.classList.add('error')
+    }else{
+        let formcontrolEl = usernameEl.parentElement;
+        formcontrolEl.classList.add('sucess')
+    }
+    if(emilEl.value === ''){
+        let formcontrolEl = emilEl.parentElement;
+        formcontrolEl.classList.add('error')
+    }else{
+        let formcontrolEl = emilEl.parentElement;
+        formcontrolEl.classList.add('sucess')
+    }
+    if(numberEl.value === ''){
+        const formcontrolEl = numberEl.parentElement;
+        formcontrolEl.class.add('error')
+    }else{
+        const formcontrolEl = numberEl.parentElement;
+        formcontrolEl.classList.add('sucess')
+    }
+    if(passwordEl.value === ''){
+        const formcontrolEl = passwordEl.parentElement;
+        formcontrolEl.classList.add('error')
+    }else{
+        const formcontrolEl = passwordEl.parentElement;
+        formcontrolEl.classList.add('sucess')
+    }
+    if(ConformPasswordel.value === ''){
+        const formcontrolEl = ConformPasswordel.parentElement;
+        formcontrolEl.classList.add('error')
+    }else{
+        const formcontrolEl = ConformPasswordel.parentElement;
+        formcontrolEl.classList.add('sucess')
+    }
+})
+
+
