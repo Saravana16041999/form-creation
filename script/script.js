fcbdcd6c87f78aeff8f54153abc109ff65425662
Saravana16041999 <saravanakumar16041999@gmail.com>
2022-09-25 19:49:08 +0530

    sdsadsad

--- script/script.js
+++ script/script.js
@@ -7,38 +7,30 @@ const numberEl = document.getElementById('number')
 const passwordEl = document.getElementById('password')
 const ConformPasswordel = document.getElementById('ConformPassword')
 
-const elements = [formEl,usernameEl,emilEl,numberEl,passwordEl,ConformPasswordel]
+const elements = [usernameEl,emilEl,numberEl,passwordEl,ConformPasswordel]
 
 const error = (el)=>{
     let formcontrolEl = el.parentElement;
     formcontrolEl.classList.add('error');
-}
+    }
+    
 const sucess = (el)=>{
     let formcontrolEl = el.parentElement;
     formcontrolEl.classList.add('sucess')
-}
+
+    }
+
+
 
 function callEverything(arr){
     for(const el of arr){
         console.log(el)
         if(el.value === ''){
-            error(el)
+                error(el)
         }else{
-            sucess(el)
+                sucess(el)
         }
-}
-}
-function username(el,max,min){
-        if (el.value < min){
-            document.querySelector('small').textContent = `name should be min of ${min}`;
-            error(el)
-        }
-        else if (el.value > max){
-            document.querySelector('small').textContent = `name should be max of ${max}`;
-            error(el)
-        }else{
-            error(el)
-        }
+    }
     }
 
 
@@ -46,6 +38,6 @@ function username(el,max,min){
 formEl.addEventListener('submit',(e)=>{
     e.preventDefault()
     callEverything(elements)
-    username(usernameEl,13,5)
+
 })
 
