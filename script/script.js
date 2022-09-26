af405e459a1e65b1dd3ccf50b2c39ae47ebf9b75
Saravana16041999 <saravanakumar16041999@gmail.com>
2022-09-26 20:08:54 +0530

    Update script.js

--- script/script.js
+++ script/script.js
@@ -9,14 +9,16 @@ const ConformPasswordel = document.getElementById('ConformPassword')
 
 const elements = [usernameEl,emilEl,numberEl,passwordEl,ConformPasswordel]
 
-const error = (el)=>{
-    let formcontrolEl = el.parentElement;
-    formcontrolEl.classList.add('error');
+const error = (el,message)=>{
+    const formControlEl = el.parentElement;
+        formControlEl.className = 'form-control error';
+        const smallEl = formControlEl.querySelector('small');
+        smallEl.textContent = message;
     }
     
 const sucess = (el)=>{
-    let formcontrolEl = el.parentElement;
-    formcontrolEl.classList.add('sucess')
+    const formControlEl = el.parentElement;
+    formControlEl.className = 'form-control sucess';
 
     }
 
@@ -24,20 +26,49 @@ const sucess = (el)=>{
 
 function callEverything(elms){
     for(const el of elms){
-        console.log(el)
         if(el.value === ''){
-                error(el)
+                error(el,`${el.name} is required`)
         }else{
                 sucess(el)
         }
     }
     }
 
-
+    const checkLength = function (el, min, max) {
+        if (el.value.length < min){
+                error(el, `${el.name}should have ${min} characters`);
+        }else if(el.value.length > max){
+                error(el, `${el.name} should not be more than ${max} characters`);
+    
+        }else{
+                sucess(el);
+        }
+    };
+    const passwordMatch = function (elOne, elTwo) {
+        if(elOne.value === '' || elTwo.value === '') {
+            error(elOne, 'new password is mandatory');
+            error(elTwo, 'confirm password is mandatory')
+        }else{
+            if(elOne.value === elTwo.value){
+                sucess(elOne);
+                sucess(elTwo);
+        }else{
+            error(elOne, 'password do not match');
+            
+        }
+        }
+            
+    };
+    
 
 formEl.addEventListener('submit',(e)=>{
     e.preventDefault()
     callEverything(elements)
+    checkLength(usernameEl, 5, 12);
+    checkLength(emailEl,5,25);
+    checkLength(numberEl,10,10);
+    checkLength(passwordEl,8,15);
+    passwordMatch(newpasswordEl, ConformPasswordel);
 
 })
 
